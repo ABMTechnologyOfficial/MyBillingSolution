5dad9f662cb4f523d9bf89bb979c15488c531443
Bhawesh <bhaweshjain384@gmail.com>
2023-01-31 00:18:36 +0530

    AddBillItemActivity Init

--- app/src/main/java/com/abmtech/mybillingsolution/ui/AddBillItemActivity.java
+++ app/src/main/java/com/abmtech/mybillingsolution/ui/AddBillItemActivity.java
@@ -2,13 +2,22 @@ package com.abmtech.mybillingsolution.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.abmtech.mybillingsolution.databinding.ActivityAddBillItemBinding;
+
 public class AddBillItemActivity extends AppCompatActivity {
+    private ActivityAddBillItemBinding binding;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_bill_item);
+        binding = ActivityAddBillItemBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        activity = this;
+
+
     }
 }
